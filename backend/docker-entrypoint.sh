c24ea63cdd40f1cad910b4bb7ba4cea9f31e723f
Kiminstir <evans.d@yandex.ru>
2026-09-08 00:23:27 +0300

    chore: add backend container, compose stack and Caddy config

---
+++ backend/docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+python manage.py bootstrap_site
+
+exec gunicorn config.wsgi:application \
+  --bind 0.0.0.0:8000 \
+  --workers "${GUNICORN_WORKERS:-3}" \
+  --access-logfile - \
+  --error-logfile -
